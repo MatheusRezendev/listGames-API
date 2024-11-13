d6a489eb3ba100b400699bf520f8a95312271c88
Matheus Reginato Rezende <contatorzn@hotmail.com>
2024-11-13 14:11:06 -0300

    Domain model

---
+++ src/main/java/org/example/listgames/entities/BelongingPK.java
@@ -0,0 +1,56 @@
+package org.example.listgames.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    public BelongingPK() {
+    }
+
+    public BelongingPK(GameList gameList, Game game) {
+        this.gameList = gameList;
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        BelongingPK that = (BelongingPK) obj;
+        return Objects.equals(gameList, that.gameList) && Objects.equals(game, that.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameList, game);
+    }
+}
